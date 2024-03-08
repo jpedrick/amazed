83edd92ab458174cb0249652db674b395509f778
Joshua Pedrick <joshua.pedrick@gmail.com>
2024-03-08 11:59:15 -0500

    cargo fmt

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -4,35 +4,38 @@ use std::collections::HashMap;
 pub struct NodeLabel(usize);
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct Neighbors<T: Clone,const MAX_NEIGHBORS: usize>([Option<T>; MAX_NEIGHBORS], usize);
+pub struct Neighbors<T: Clone, const MAX_NEIGHBORS: usize>([Option<T>; MAX_NEIGHBORS], usize);
 
 #[derive(Debug, PartialEq)]
 pub enum AddEdgeError {
     Overflow,
-    Exists
+    Exists,
 }
 
 pub trait KeyTraits: Clone + Ord + Copy + std::hash::Hash {}
 impl<T: Clone + Ord + Copy + std::hash::Hash + ?Sized> KeyTraits for T {}
 
-pub fn option_less_than<T:KeyTraits>(p: &Option<T>,q: &Option<T>) -> std::cmp::Ordering{
-    match (p,q) {
+pub fn option_less_than<T: KeyTraits>(p: &Option<T>, q: &Option<T>) -> std::cmp::Ordering {
+    match (p, q) {
         (Some(_), None) => std::cmp::Ordering::Less,
         (None, Some(_)) => std::cmp::Ordering::Greater,
         (None, None) => std::cmp::Ordering::Equal,
-        (Some(x), Some(y)) => x.cmp(y)
+        (Some(x), Some(y)) => x.cmp(y),
     }
-} 
+}
 
-impl<T:KeyTraits,const MAX_NEIGHBORS: usize> Neighbors<T,MAX_NEIGHBORS> {
+impl<T: KeyTraits, const MAX_NEIGHBORS: usize> Neighbors<T, MAX_NEIGHBORS> {
     pub fn append(&mut self, node_id: T) -> Result<&mut Self, AddEdgeError> {
         if self.1 < MAX_NEIGHBORS {
-            match self.0.binary_search_by(|x| option_less_than(x,&Some(node_id))) {
+            match self
+                .0
+                .binary_search_by(|x| option_less_than(x, &Some(node_id)))
+            {
                 Ok(_) => Err(AddEdgeError::Exists),
                 Err(pos) => {
                     if pos != self.1 {
-                        self.0.copy_within(pos..self.1, pos+1);
-                    } 
+                        self.0.copy_within(pos..self.1, pos + 1);
+                    }
                     self.0[pos] = Some(node_id);
                     self.1 += 1;
 
@@ -45,39 +48,46 @@ impl<T:KeyTraits,const MAX_NEIGHBORS: usize> Neighbors<T,MAX_NEIGHBORS> {
     }
 }
 
-impl<T:KeyTraits,const N: usize, const MAX_NEIGHBORS: usize> From<&[T; N]> for Neighbors<T,MAX_NEIGHBORS> {
-    fn from(neighbors: &[T; N]) -> Neighbors<T,MAX_NEIGHBORS> {
+impl<T: KeyTraits, const N: usize, const MAX_NEIGHBORS: usize> From<&[T; N]>
+    for Neighbors<T, MAX_NEIGHBORS>
+{
+    fn from(neighbors: &[T; N]) -> Neighbors<T, MAX_NEIGHBORS> {
         let mut ns = Neighbors([None; MAX_NEIGHBORS], N);
 
-        ns.0[0..N].copy_from_slice( &neighbors.map(|x|Some(x)) );
+        ns.0[0..N].copy_from_slice(&neighbors.map(|x| Some(x)));
 
         ns
     }
 }
 
-impl<T:KeyTraits,const MAX_NEIGHBORS: usize> From<&[Option<T>; MAX_NEIGHBORS]> for Neighbors<T,MAX_NEIGHBORS> {
-    fn from(neighbors: &[Option<T>; MAX_NEIGHBORS]) -> Neighbors<T,MAX_NEIGHBORS> {
-        let first_null = neighbors.iter().position(|x| x.is_none()).unwrap_or(MAX_NEIGHBORS);
+impl<T: KeyTraits, const MAX_NEIGHBORS: usize> From<&[Option<T>; MAX_NEIGHBORS]>
+    for Neighbors<T, MAX_NEIGHBORS>
+{
+    fn from(neighbors: &[Option<T>; MAX_NEIGHBORS]) -> Neighbors<T, MAX_NEIGHBORS> {
+        let first_null = neighbors
+            .iter()
+            .position(|x| x.is_none())
+            .unwrap_or(MAX_NEIGHBORS);
 
-        Neighbors::<T,MAX_NEIGHBORS>(*neighbors, first_null)
+        Neighbors::<T, MAX_NEIGHBORS>(*neighbors, first_null)
     }
 }
 
-pub type GraphNodes<T,const MAX_NEIGHBORS: usize> = HashMap<T, Neighbors<T,MAX_NEIGHBORS>>;
+pub type GraphNodes<T, const MAX_NEIGHBORS: usize> = HashMap<T, Neighbors<T, MAX_NEIGHBORS>>;
 
 #[derive(Clone, Debug)]
-pub struct Graph<T:KeyTraits,const MAX_NEIGHBORS: usize>(GraphNodes<T,MAX_NEIGHBORS>);
+pub struct Graph<T: KeyTraits, const MAX_NEIGHBORS: usize>(GraphNodes<T, MAX_NEIGHBORS>);
 
-impl<T:KeyTraits,const MAX_NEIGHBORS: usize> std::ops::Deref for Graph<T,MAX_NEIGHBORS> {
-    type Target = GraphNodes<T,MAX_NEIGHBORS>;
+impl<T: KeyTraits, const MAX_NEIGHBORS: usize> std::ops::Deref for Graph<T, MAX_NEIGHBORS> {
+    type Target = GraphNodes<T, MAX_NEIGHBORS>;
 
-    fn deref(&self) -> &GraphNodes<T,MAX_NEIGHBORS> {
+    fn deref(&self) -> &GraphNodes<T, MAX_NEIGHBORS> {
         &self.0
     }
 }
 
-impl<T:KeyTraits,const MAX_NEIGHBORS: usize> std::ops::DerefMut for Graph<T,MAX_NEIGHBORS> {
-    fn deref_mut(&mut self) -> &mut GraphNodes<T,MAX_NEIGHBORS> {
+impl<T: KeyTraits, const MAX_NEIGHBORS: usize> std::ops::DerefMut for Graph<T, MAX_NEIGHBORS> {
+    fn deref_mut(&mut self) -> &mut GraphNodes<T, MAX_NEIGHBORS> {
         &mut self.0
     }
 }
@@ -85,10 +95,10 @@ impl<T:KeyTraits,const MAX_NEIGHBORS: usize> std::ops::DerefMut for Graph<T,MAX_
 #[derive(Debug, PartialEq)]
 pub enum InsertEdgeError {
     FromMissing,
-    AppendEdgeError(AddEdgeError)
+    AppendEdgeError(AddEdgeError),
 }
 
-impl<T:KeyTraits,const MAX_NEIGHBORS: usize> Graph<T,MAX_NEIGHBORS> {
+impl<T: KeyTraits, const MAX_NEIGHBORS: usize> Graph<T, MAX_NEIGHBORS> {
     pub fn new() -> Self {
         Graph(GraphNodes::new())
     }
@@ -101,7 +111,7 @@ impl<T:KeyTraits,const MAX_NEIGHBORS: usize> Graph<T,MAX_NEIGHBORS> {
         if let Some(from_node) = self.0.get_mut(&from) {
             match from_node.append(to) {
                 Ok(_) => Ok(self),
-                Err(e) => Err(InsertEdgeError::AppendEdgeError(e))
+                Err(e) => Err(InsertEdgeError::AppendEdgeError(e)),
             }
         } else {
             Err(InsertEdgeError::FromMissing)
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-pub mod graph;
 pub mod geometry;
+pub mod graph;
 
 #[cfg(test)]
 mod tests {
@@ -8,15 +8,18 @@ mod tests {
 
     #[test]
     fn test_insert() {
-        let mut g = Graph::<usize,6>::new();
+        let mut g = Graph::<usize, 6>::new();
         g.insert(1, &[2, 3, 4, 5]);
         g.insert(2, &[1, 3, 4]);
         g.insert(3, &[2, 3, 4]);
         g.insert(4, &[1, 2, 3]);
         g.insert(5, &[1]);
 
-        let s = graph::GraphNodes::<usize,6>::from([
-            (1, (&[Some(2), Some(3), Some(4), Some(5), None, None]).into()),
+        let s = graph::GraphNodes::<usize, 6>::from([
+            (
+                1,
+                (&[Some(2), Some(3), Some(4), Some(5), None, None]).into(),
+            ),
             (2, (&[Some(1), Some(3), Some(4), None, None, None]).into()),
             (3, (&[Some(2), Some(3), Some(4), None, None, None]).into()),
             (4, (&[Some(1), Some(2), Some(3), None, None, None]).into()),
@@ -27,7 +30,7 @@ mod tests {
 
     #[test]
     fn test_add_edge() {
-        let mut g = Graph::<usize,6>::new();
+        let mut g = Graph::<usize, 6>::new();
         g.insert(1, &[2, 3, 4, 5]);
         g.insert(2, &[1, 3, 4]);
         g.insert(3, &[2, 3, 4]);
@@ -35,10 +38,18 @@ mod tests {
         g.insert(5, &[1]);
         g.insert_edge(5, 3).expect("This should be ok");
         g.insert_edge(5, 2).expect("This should be ok");
-        assert_eq!(g.insert_edge(5, 2).err(), Some(crate::graph::InsertEdgeError::AppendEdgeError(crate::graph::AddEdgeError::Exists)));
+        assert_eq!(
+            g.insert_edge(5, 2).err(),
+            Some(crate::graph::InsertEdgeError::AppendEdgeError(
+                crate::graph::AddEdgeError::Exists
+            ))
+        );
 
-        let s = graph::GraphNodes::<usize,6>::from([
-            (1, (&[Some(2), Some(3), Some(4), Some(5), None, None]).into()),
+        let s = graph::GraphNodes::<usize, 6>::from([
+            (
+                1,
+                (&[Some(2), Some(3), Some(4), Some(5), None, None]).into(),
+            ),
             (2, (&[Some(1), Some(3), Some(4), None, None, None]).into()),
             (3, (&[Some(2), Some(3), Some(4), None, None, None]).into()),
             (4, (&[Some(1), Some(2), Some(3), None, None, None]).into()),
@@ -49,7 +60,7 @@ mod tests {
 
     #[test]
     fn test_overflow_node() {
-        let mut g = Graph::<usize,6>::new();
+        let mut g = Graph::<usize, 6>::new();
         g.insert(1, &[2, 3, 4, 5]);
         g.insert(2, &[1, 3, 4]);
         g.insert(3, &[2, 3, 4]);
@@ -60,21 +71,28 @@ mod tests {
         g.insert(8, &[1]);
         g.insert_edge(1, 6).expect("Ok");
         g.insert_edge(1, 7).expect("Ok");
-        assert_eq!(g.insert_edge(1, 8).err(), Some(crate::graph::InsertEdgeError::AppendEdgeError(crate::graph::AddEdgeError::Overflow)));
-        assert_eq!(g.insert_edge(9, 1).err(), Some(crate::graph::InsertEdgeError::FromMissing));
+        assert_eq!(
+            g.insert_edge(1, 8).err(),
+            Some(crate::graph::InsertEdgeError::AppendEdgeError(
+                crate::graph::AddEdgeError::Overflow
+            ))
+        );
+        assert_eq!(
+            g.insert_edge(9, 1).err(),
+            Some(crate::graph::InsertEdgeError::FromMissing)
+        );
     }
 
     #[test]
     fn test_tuple_nodes() {
-        let mut g = Graph::<(usize, usize),2>::new();
-        g.insert((0,0), &[(0,1)]);
-        g.insert((0,1), &[(0,0)]);
+        let mut g = Graph::<(usize, usize), 2>::new();
+        g.insert((0, 0), &[(0, 1)]);
+        g.insert((0, 1), &[(0, 0)]);
 
-        let s = graph::GraphNodes::<(usize, usize),2>::from([
-            ((0,0), (&[Some((0,1)), None]).into()),
-            ((0,1), (&[Some((0,0)), None]).into())
+        let s = graph::GraphNodes::<(usize, usize), 2>::from([
+            ((0, 0), (&[Some((0, 1)), None]).into()),
+            ((0, 1), (&[Some((0, 0)), None]).into()),
         ]);
         assert_eq!(*g, s);
     }
 }
-
